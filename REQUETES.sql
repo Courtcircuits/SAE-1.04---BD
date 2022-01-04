5c35c66f7f77f36980f63005f69de0ee64000af2
Nayggets <79973358+Nayggets@users.noreply.github.com>
2022-01-04 18:36:23 +0100

    Update REQUETES.sql

--- REQUETES.sql
+++ REQUETES.sql
@@ -1,3 +1,9 @@
 /*Récupère la somme des prix des commandes passées par le client 10*/
 SELECT SUM(prixCommande) FROM Commande 
 WHERE idClient=10;
+
+/*Renvoie tout les noms de fournisseur qui fournisse un produit*/
+SELECT nomFournisseur
+FROM Fournisseur
+where identifiantFournisseur IN (SELECT identifiantFournisseur
+                                 FROM Fournir);
