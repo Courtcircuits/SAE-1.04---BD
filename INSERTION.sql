90195c50c9904c0e597896aa076bbdb1e458abb6
Nayggets <79973358+Nayggets@users.noreply.github.com>
2022-01-04 17:35:53 +0100

    Update INSERTION.sql

--- INSERTION.sql
+++ INSERTION.sql
@@ -41,3 +41,8 @@ INSERT INTO Fournisseur VALUES (3, 'Basse inné');
 INSERT INTO Fournisseur VALUES (4, 'fauré');
 INSERT INTO Fournisseur VALUES (5, 'un bon Grieg et au liszt');
 INSERT INTO Fournisseur VALUES (6, 'Le DoRé');
+INSERT INTO Fournisseur VALUES (7, 'Satie bien');
+INSERT INTO Fournisseur VALUES (8, 'Bet Hoven');
+INSERT INTO Fournisseur VALUES (9, 'Tronc et Bonne');
+INSERT INTO Fournisseur VALUES (10, 'La Ré du Do');
+
