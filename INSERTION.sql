78f26bd7d940d22930b7292f95dc08caf54475a6
Sliimsey <91788506+Sliimsey@users.noreply.github.com>
2022-01-04 17:15:15 +0100

    Update INSERTION.sql

--- INSERTION.sql
+++ INSERTION.sql
@@ -9,3 +9,15 @@ INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALU
 INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (8, "VIP")
 INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (9, "Magasin physique")
 INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (10, "Autres")
+
+/*Departement*/
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (09, 'Ariège', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (11, 'Aude', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (12, 'Aveyron', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (30, 'Gard', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (31, 'Haute-Garonne', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (32, 'Gers', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (34, 'Hérault', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (46, 'Lot', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (81, 'Tarn', 'Occitanie');
+INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (48, 'Lozère', 'Occitanie');
