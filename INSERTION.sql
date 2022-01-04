da44f950512ee573ff341c1a7a3f3875a0c0076a
Milou <90451752+Milou666@users.noreply.github.com>
2022-01-04 17:27:48 +0100

    Update INSERTION.sql

--- INSERTION.sql
+++ INSERTION.sql
@@ -1,14 +1,14 @@
 ﻿/*Catégorie Client*/
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (1, "DJ");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (2, "Professeur de musique");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (3, "Musicien amateur");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (4, "Musicien professionel");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (5, "Débutant");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (6, "Association");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (7, "Ecole de musique");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (8, "VIP");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (9, "Magasin physique");
-INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (10, "Autres");
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (1, 'DJ');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (2, 'Professeur de musique');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (3, 'Musicien amateur');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (4, 'Musicien professionel');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (5, 'Débutant');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (6, 'Association');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (7, 'Ecole de musique');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (8, 'VIP');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (9, 'Magasin physique');
+INSERT INTO CategorieClient(identifiantCategorieClient, nomCategorieClient) VALUES (10, 'Autres');
 
 /*Departement*/
 INSERT INTO Departement(identifiantDepartement, nomDepartement, nomRegion) VALUES (09, 'Ariège', 'Occitanie');
