9ecaabf99af16033b0da7fab343f1742e443f09d
Luckyponk <95107804+Luckyponk@users.noreply.github.com>
2022-01-11 15:35:33 +0100

    Update MODIF-TABLE.sql

--- MODIF-TABLE.sql
+++ MODIF-TABLE.sql
@@ -1,10 +1,13 @@
+/*Création de la table FournisseurPrincipal*/
 CREATE TABLE FournisseurPrincipal
 (idProduit NUMBER(5,0), identifiantFournisseur NUMBER(5,0),
 CONSTRAINT pk_FournisseurPrincipal PRIMARY KEY (idProduit, identifiantFournisseur),
 CONSTRAINT fk_FournisseurPrincipal FOREIGN KEY (idProduit, identifiantFournisseur) REFERENCES Fournir(idProduit, identifiantFournisseur));
 
+/*Création de la table Jour*/
 CREATE TABLE Jour
 (jour DATE,
 CONSTRAINT pk_Jour PRIMARY KEY(jour));
 
+/*Ajout de l'attribut jour dans la table SeTrouver*/
 ALTER TABLE SeTrouver ADD jour DATE;
