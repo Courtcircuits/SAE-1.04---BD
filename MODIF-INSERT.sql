bbc9e71d5f9825c6afdfbcccefcc980f63f71678
Luckyponk <95107804+Luckyponk@users.noreply.github.com>
2022-01-11 15:31:27 +0100

    Update MODIF-INSERT.sql

--- MODIF-INSERT.sql
+++ MODIF-INSERT.sql
@@ -21,3 +21,18 @@ UPDATE SeTrouver SET jour='17/01/2021' WHERE idCommande=6 and idProduit=9;
 UPDATE SeTrouver SET jour='12/01/2021' WHERE idCommande=7 and idProduit=8;
 UPDATE SeTrouver SET jour='19/01/2021' WHERE idCommande=8 and idProduit=7;
 UPDATE SeTrouver SET jour='21/01/2021' WHERE idCommande=9 and idProduit=6;
+
+/* Ajout d'un tuple dans fournir pour montrer qu'un fournisseur peut être le fournisseur principal de plusieurs produits*/
+INSERT INTO Fournir (idProduit, identifiantFournisseur) VALUES (9,2);
+
+/* Ajout des tuples dans FournisseurPrincipal*/
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (0,1);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (9,2);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (3,2);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (4,9);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (1,10);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (2,4);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (5,6);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (6,8);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (7,5);
+INSERT INTO FournisseurPrincipal (idProduit, identifiantFournisseur) VALUES (8,3);
