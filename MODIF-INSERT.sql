abce6bf314043e69da7a7b095be7266d0eb2ee8c
Luckyponk <95107804+Luckyponk@users.noreply.github.com>
2022-01-11 15:15:43 +0100

    Update MODIF-INSERT.sql

--- MODIF-INSERT.sql
+++ MODIF-INSERT.sql
@@ -10,17 +10,6 @@ INSERT INTO Jour(jour) VALUES ('19/01/2021');
 INSERT INTO Jour(jour) VALUES ('20/01/2021');
 INSERT INTO Jour(jour) VALUES ('21/01/2021');
 
-/* Modification de la table Produit */
-UPDATE Produit SET idFournisseurPrincipal=1 WHERE idProduit=0;
-UPDATE Produit SET idFournisseurPrincipal=2 WHERE idProduit=3;
-UPDATE Produit SET idFournisseurPrincipal=9 WHERE idProduit=4;
-UPDATE Produit SET idFournisseurPrincipal=10 WHERE idProduit=1;
-UPDATE Produit SET idFournisseurPrincipal=4 WHERE idProduit=2;
-UPDATE Produit SET idFournisseurPrincipal=6 WHERE idProduit=5;
-UPDATE Produit SET idFournisseurPrincipal=8 WHERE idProduit=6;
-UPDATE Produit SET idFournisseurPrincipal=5 WHERE idProduit=7;
-UPDATE Produit SET idFournisseurPrincipal=3 WHERE idProduit=8;
-
 /* SeTrouver */
 UPDATE SeTrouver SET jour='12/01/2021' WHERE idCommande=1 and idProduit=3;
 UPDATE SeTrouver SET jour='13/01/2021' WHERE idCommande=1 and idProduit=2;
